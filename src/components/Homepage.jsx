667b16237539053796cc87847b1b38aaf26fa50f
Yhethi <eloyjosebonillag@gmail.com>
2024-01-02 14:22:14 -0400

    v1.073

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/css/Body.css";
 import { Hero } from "./Hero";
 import { Notices } from "./Notices";
@@ -8,25 +8,29 @@ import videoHome from "../assets/body/Aetheria1.mp4";
 
 export const Homepage = ({ PlaySound }) => {
   function init() {
-    let txt = document.createTextNode(data[index]);
-    section.dataset.identity = data[index];
-    span.innerText = txt.textContent;
-    index++;
-  }
-  setTimeout(() => {
     let index = 0;
     let data = ["Unete al Universo"];
     let span = document.querySelector("span");
     let section = document.querySelector("section");
-    init();
-    setInterval(function () {
-      let txt = document.createTextNode(data[index]);
-      section.dataset.identity = data[index];
-      span.innerText = txt.textContent;
-      index++;
-      index = index < data.length ? index++ : 0;
-    }, 4501);
-  }, 500);
+
+    // let txt = document.createTextNode(data[index]);
+    // section.dataset.identity = data[index];
+    // span.innerText = txt.textContent;
+    // index++;
+
+    // setInterval(function () {
+    //   let txt = document.createTextNode(data[index]);
+    //   section.dataset.identity = data[index];
+    //   span.innerText = txt.textContent;
+    //   index++;
+    //   index = index < data.length ? index++ : 0;
+    // }, 4501);
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      init();
+    }, 500);
+  }, []);
 
   const mousemove = (event) => {
     const backVideo = document.querySelector(".backVideo");
@@ -59,10 +63,10 @@ export const Homepage = ({ PlaySound }) => {
         <div className="lettersAnimation">
           <section className="sectionHuman" data-identity="UNETE AL UNIVERSO">
             <blockquote className="d-flex">
-              <p className="d-flex">
+              <div className="d-flex">
                 HUMAN
                 <p style={{ color: "#FFF", marginLeft: "7px" }}>UNLEASH</p>
-              </p>
+              </div>
               <span></span>
             </blockquote>
           </section>
